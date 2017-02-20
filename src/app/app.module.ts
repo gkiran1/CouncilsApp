8cd55818dd61a491a9a7ae47c627c71f50d65b60
KambagiriSwamyB <bkambagiri@prokarma.com>
2017-02-20 19:05:52 +0530

    setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { AuthService } from '../providers/auth-service';
+import { RegisterPage } from '../pages/register/register';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseConfig } from './../environments/firebase/firebase-config';
+import { FirebaseService } from './../environments/firebase/firebase-service';
+import { DisplayPage } from '../pages/display/display';
+import { CouncilLoginPage } from '../pages/councillogin/councillogin';
+import * as firebase from 'firebase';
+import { WelcomePage } from '../pages/welcome/welcome';
+import { CreateAccountPage } from '../pages/create-account/create-account';
+import { NewAssignmentPage } from '../pages/new-assignment/new-assignment';
+import { AppService } from '../providers/app-service';
+import { Elastic } from '../directives/elastic'; 
+import { NewAgenda } from '../pages/new-agenda/new-agenda';
+import { InviteMemberPage } from '../pages/invite/invite';
+import { InvitationSuccessPage } from '../pages/invite/success';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    DisplayPage,
+    CouncilLoginPage,
+    WelcomePage,
+    CreateAccountPage,
+    NewAssignmentPage,
+    Elastic,
+    NewAgenda,
+    InviteMemberPage,
+    InvitationSuccessPage
+   
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FirebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    DisplayPage,
+    CouncilLoginPage,
+    WelcomePage,
+    CreateAccountPage,
+    NewAssignmentPage,
+    NewAgenda,
+    InviteMemberPage,
+    InvitationSuccessPage
+
+  ],
+  providers: [AuthService,AppService,FirebaseService]
+})
+export class AppModule { }
---
+++ src/environments/firebase/firebase-config.ts
@@ -0,0 +1,13 @@
+export const FirebaseConfig = {
+    // apiKey: "AIzaSyCCZUW7XFRgKj2BMokYsMqCYVdc6qSvO_8",
+    // authDomain: "simplelogin-5d2a3.firebaseapp.com",
+    // databaseURL: "https://simplelogin-5d2a3.firebaseio.com",
+    // storageBucket: "simplelogin-5d2a3.appspot.com",
+    // messagingSenderId: "921011060967"
+
+    apiKey: "AIzaSyDauCRlokcRecT283636ODNP2z4qZe2oPA",
+    authDomain: "lds-councils-8f56b.firebaseapp.com",
+    databaseURL: "https://lds-councils-8f56b.firebaseio.com",
+    storageBucket: "lds-councils-8f56b.appspot.com",
+    messagingSenderId: "619253720821"
+}
