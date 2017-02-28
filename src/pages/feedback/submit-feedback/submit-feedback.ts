5f50cfd57854d3eb156e2bc36035495663a63418
tbharati <tbharati@prokarma.com>
2017-02-28 11:47:48 +0530

    no message

--- src/pages/feedback/submit-feedback/submit-feedback.ts
+++ src/pages/feedback/submit-feedback/submit-feedback.ts
@@ -3,22 +3,23 @@ import { NavController } from 'ionic-angular';
 import { FirebaseService } from '../../../environments/firebase/firebase-service';
 import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 import { ThanksFeedbackPage } from '../thanks-feedback/thanks-feedback';
+import { AppService } from '../../../providers/app-service';
 
 @Component({
     selector: 'submit-feedback',
     templateUrl: 'submit-feedback.html',
-    providers: [FirebaseService]
+    providers: [FirebaseService, AppService]
 })
 
 export class SubmitFeedbackPage {
 
     public feedback: string;
 
-    constructor(public navCtrl: NavController, private ser: FirebaseService) {
+    constructor(public navCtrl: NavController, private ser: FirebaseService, private appSer: AppService) {
     }
 
     submitFeedback() {
-        this.ser.saveFeedback(this.feedback, 'user1', Date.now());
+        this.ser.saveFeedback(this.feedback, this.appSer.uid, new Date().toDateString());
         this.navCtrl.push(ThanksFeedbackPage);
     }
 
