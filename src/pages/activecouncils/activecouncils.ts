d4bab40302c070d645716c4323cacbe98d7709d1
tbharati <tbharati@prokarma.com>
2017-02-28 15:05:59 +0530

    Using users instead of usercouncils..

--- src/pages/activecouncils/activecouncils.ts
+++ src/pages/activecouncils/activecouncils.ts
@@ -4,8 +4,8 @@ import { FirebaseService } from '../../environments/firebase/firebase-service';
 import { AppService } from '../../providers/app-service';
 import * as firebase from 'firebase';
 import { User } from '../../user/user';
-import { Observable,Subject } from 'rxjs/Rx';
-import { AngularFire, FirebaseObjectObservable,FirebaseListObservable } from 'angularfire2';
+import { Observable, Subject } from 'rxjs/Rx';
+import { AngularFire, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
 import { Council } from '../new-council/council';
 
 @Component({
@@ -15,29 +15,22 @@ import { Council } from '../new-council/council';
 
 export class ActiveCouncilsPage {
     users: any;
-    //  newCouncil: Council = new Council();
     myCouncils = [];
     count$ = new Subject();
 
     constructor(public af: AngularFire, public firebaseservice: FirebaseService, public appservice: AppService) {
         this.appservice.getUser().subscribe(user => {
-
-            this.firebaseservice.getUserCounilKeysByUserKey(user.$key).subscribe((usercouncil) => {
-                console.log('usercouncils', usercouncil);
-                this.myCouncils = [];
-                usercouncil.forEach(e => {
-                    //  this.myCouncils.push(this.firebaseservice.getCouncilByKey(e.councilid))
-                   this.firebaseservice.getCouncilByKey(e.councilid).subscribe(councilObj=>{
+            this.myCouncils = [];
+            user.councils.forEach(e => {
+                this.firebaseservice.getCouncilByKey(e).subscribe(councilObj => {
                     this.myCouncils.push(...councilObj);
-                    this.count$.next(this.myCouncils.length);
-                   });
-                   
+                    this.count$.next(user.councils.length);
                 });
             });
-      
         });
     }
-    getCount(){
+
+    getCount() {
         return this.count$;
     }
 
