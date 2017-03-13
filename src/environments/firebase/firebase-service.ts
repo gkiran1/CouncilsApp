73b3b9f700c66d9aef2b89b83d40cdf7d977d417
JhansiLella <ljhansi@prokarma.com>
2017-03-13 18:32:56 +0530

    added the code to upload picture in edit profile page

--- src/environments/firebase/firebase-service.ts
+++ src/environments/firebase/firebase-service.ts
@@ -55,7 +55,8 @@ export class FirebaseService {
                 createdby: user.createdby,
                 createddate: user.createddate,
                 lastupdateddate: user.lastupdateddate,
-                isactive: user.isactive
+                isactive: user.isactive,
+                guestpicture: user.avatar
             }).then(() => user.councils.forEach(counc => {
                 this.createUserCouncils(uid, counc);
             }));
@@ -328,8 +329,15 @@ export class FirebaseService {
         console.log('assignment.$key', assignmentKey);
         return this.af.database.object('assignments/' + assignmentKey).remove();
     }
-    updateProfile(userUid: string, firstname, lastname, email, phone, ldsusername) {
-        return this.rootRef.child('users/' + userUid).update({ firstname, lastname, email, phone, ldsusername }).then(() => {
+    updateProfile(userUid: string, firstname, lastname, email, phone, ldsusername, guestpicture) {
+        return this.rootRef.child('users/' + userUid).update({ firstname, lastname, email, phone, ldsusername, guestpicture }).then(() => {
+            return "user profile updated successfully..."
+        }).catch(err => {
+            throw err;
+        })
+    }
+    setAvatarInUser(avatar: any, userUid: string) {
+        return this.rootRef.child('users/' + userUid).update({ avatar: avatar }).then(() => {
             return "user profile updated successfully..."
         }).catch(err => {
             throw err;
