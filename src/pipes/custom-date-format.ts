eaf1de1b6ad90c0e721643be09d33866713f1021
KambagiriSwamyB <bkambagiri@prokarma.com>
2017-03-16 12:55:12 +0530

    added missing date pipe

---
+++ src/pipes/custom-date-format.ts
@@ -0,0 +1,15 @@
+import { Injectable, Pipe } from '@angular/core';
+import * as moment from 'moment';
+@Pipe({
+  name: 'customdateformat',
+  pure: true
+})
+@Injectable()
+export class CustomDateFormat {
+  transform(value, args) {
+    if(!moment(value).isValid()) return value; // checking for invalid dates
+    value = moment(value).calendar(); // apply calendar format initially
+    if(moment(value,'MM/DD/YYYY',true).isValid()) return moment(value).format('LL'); // apply full date format
+    return value;
+  }
+}
