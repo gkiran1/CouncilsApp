6493707a54f29bdb14fd5be700255b4a8864f215
tbharati <tbharati@prokarma.com>
2017-03-16 15:05:22 +0530

    Added ADmin Page in new menu

--- src/pages/slide1/slide1.ts
+++ src/pages/slide1/slide1.ts
@@ -6,11 +6,22 @@ import { Observable } from 'rxjs/Observable';
 import { WelcomePage } from '../menu/menu';
 import { AgendaPage } from '../agenda/agenda';
 import { NewBlankAgendaPage } from '../new-blankagenda/new-blankagenda';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { AdminPage } from '../admin/admin.component';
 
 @Component({
     templateUrl: 'slide1.html',
     selector: 'slide1'
 })
+
 export class slide1Page {
 
+    constructor(public nav: NavController,
+        public af: AngularFire) {
+
+    }
+
+    adminPage() {
+        this.nav.setRoot(AdminPage);
+    }
 }
