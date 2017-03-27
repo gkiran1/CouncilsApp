593c7f253a694c607a39f0d08455e24ad0bca207
gkiran1 <gkiran@prokarma.com>
2017-03-27 11:39:06 +0530

    Menu slied to 1st page only for the 1st time.

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -104,11 +104,15 @@ export class WelcomePage implements OnInit {
   ngOnInit() {
   }
   menuOpened() {
-    this.switcher.update();
-    setTimeout(() => {
+    
+      setTimeout(() => { 
       this.switcher.update();
-      this.switcher.slideTo(1, 0);
-    }, 300);
+      if(localStorage.getItem('isMenuCentered') === '0') {
+        localStorage.setItem('isMenuCentered','1');
+        this.switcher.slideTo(1,0);
+      }
+    },300);
+
   }
 
   councilsPage() {
