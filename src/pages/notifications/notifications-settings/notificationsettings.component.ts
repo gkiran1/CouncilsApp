b28a161ddd58f17af9a1b2d1dd0bc75b0dd98e9d
tbharati <tbharati@prokarma.com>
2017-04-03 12:20:34 +0530

    Added new page - Notification Settings

---
+++ src/pages/notifications/notifications-settings/notificationsettings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavParams, NavController } from 'ionic-angular';
+
+@Component({
+    templateUrl: 'notificationsettings.html',
+    selector: 'notificationsettings-page'
+})
+
+export class NotificationSettingsPage {
+}
