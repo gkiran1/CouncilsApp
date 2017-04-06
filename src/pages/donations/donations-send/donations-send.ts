b4bb53c94c6dcfd14e2c240063da9b1a7108e1fb
KambagiriSwamyB <bkambagiri@prokarma.com>
2017-04-06 17:28:17 +0530

    changes in donations

--- src/pages/donations/donations-send/donations-send.ts
+++ src/pages/donations/donations-send/donations-send.ts
@@ -16,7 +16,7 @@ export class DonationsSendPage {
       fullname: ['', Validators.required],
       email: ['', Validators.compose([Validators.required, validateEmail])],
       donationtype: ['onetime', Validators.required],
-      creditcardNo: ['', Validators.compose([Validators.required, Validators.minLength(19), Validators.maxLength(19)])],
+      creditcardNo: ['', Validators.compose([Validators.required, Validators.minLength(18), Validators.maxLength(18)])],
       creditValidthru: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(5)])],
     });
   }
@@ -39,17 +39,32 @@ export class DonationsSendPage {
     (<FormControl>this.donationForm.controls['amount']).setValue(amount);
   }
   formatToCreditNumber(event) {
+    let start = event.target.selectionStart;
+    // let end = event.target.selectionEnd;
     let cardNumber = event.target.value.replace(/[^0-9]/g, '');
     cardNumber = cardNumber.match(/.{1,4}/g);
     cardNumber = cardNumber ? cardNumber.join('-') : cardNumber;
-    (<FormControl>this.donationForm.controls['creditcardNo']).setValue(cardNumber);
+    event.target.value = cardNumber;
+    if (start === 5) {
+      start = 6;
+    } else if (start === 10) {
+      start = 11;
+    } else if (start === 15) {
+      start = 16;
+    }
+    event.target.setSelectionRange(start, start); // to set cursor position for inline editing - JS method 
   }
   formatToValidThru(event) {
+    let start = event.target.selectionStart;
     let validthru = event.target.value.replace(/[^0-9]/g, '');
     validthru = /^[^01]/.test(validthru) ? '0' + validthru : validthru;
     validthru = /1[^012]/.test(validthru.substr(0, 2)) ? validthru.replace(validthru.charAt(1), '') : validthru;
     validthru = validthru.match(/.{1,2}/g);
     validthru = validthru ? validthru.join('/') : validthru;
-    (<FormControl>this.donationForm.controls['creditValidthru']).setValue(validthru);
+    event.target.value = validthru;
+    if (start === 3) {
+      start = 4;
+    }
+    event.target.setSelectionRange(start, start);
   }
 }
