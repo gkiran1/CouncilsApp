1ee4c1275feb3dc2c677e50a1a8a0deb12b94b18
vanjanishyam <vanjanisyam@prokarma.com>
2017-04-24 17:36:54 +0530

    change of date format.

--- src/pages/notes/notes/notes.ts
+++ src/pages/notes/notes/notes.ts
@@ -7,6 +7,7 @@ import { NewNotePage } from '../../notes/newnote/newnote';
 import { NotePage } from '../../notes/note/note';
 import { Subject } from 'rxjs/Subject';
 import { NotificationsPage } from '../../notifications/notifications-page/notifications.component';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
     templateUrl: 'notes.html',
@@ -17,18 +18,32 @@ export class NotesPage {
     notesArray = [];
     count$ = new Subject();
     notificationsCount;
+    userSubscription;
 
-    constructor(public nav: NavController, public as: AppService, public firebaseservice: FirebaseService) {
-        var userId = localStorage.getItem('securityToken');
-        if (userId !== null) {
-            this.notesArray = [];
-            this.firebaseservice.getNotes(userId).subscribe(notes => {
-                this.notesArray = notes;
-                let length = this.notesArray.length;
-                length = length ? length : null;
-                this.count$.next(length);
-            });
-        }
+    constructor(public nav: NavController, public af: AngularFire, public as: AppService, public firebaseservice: FirebaseService) {
+
+        this.userSubscription = this.af.auth.subscribe(auth => {
+            if (auth !== null) {
+                this.notesArray = [];
+                this.firebaseservice.getNotes(auth.uid).subscribe(notes => {
+                    this.notesArray = notes;
+                    let length = this.notesArray.length;
+                    length = length ? length : null;
+                    this.count$.next(length);
+                });
+            }
+        });
+
+        // var userId = localStorage.getItem('securityToken');
+        // if (userId !== null) {
+        //     this.notesArray = [];
+        //     this.firebaseservice.getNotes(userId).subscribe(notes => {
+        //         this.notesArray = notes;
+        //         let length = this.notesArray.length;
+        //         length = length ? length : null;
+        //         this.count$.next(length);
+        //     });
+        // }
 
         firebaseservice.getNotCnt().subscribe(count => {
             this.notificationsCount = count;
