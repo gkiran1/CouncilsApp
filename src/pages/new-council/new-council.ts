94eb710f5bce6b0234a773cfe9b8166b6e78cb79
vanjanishyam <vanjanisyam@prokarma.com>
2017-04-28 11:35:10 +0530

    committing new-council pages.

--- src/pages/new-council/new-council.ts
+++ src/pages/new-council/new-council.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { FirebaseConfig } from '../../environments/firebase/firebase-config';
 import { FirebaseService } from '../../environments/firebase/firebase-service';
-import { AppService } from '../../providers/app-service';
+
 import * as firebase from 'firebase';
 import { User } from '../../user/user';
 import { Observable } from 'rxjs/Rx';
 import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 import { Council } from './council'
 import { AlertController, NavController } from 'ionic-angular';
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'new-council',
+  selector: 'new-createcouncil',
   templateUrl: 'new-council.html'
 })
 
@@ -19,26 +20,31 @@ export class NewCouncilPage {
   users: any;
   newCouncil: Council = new Council();
   userCouncils: any;
+  userSubscription: Subscription;
 
-  constructor(public af: AngularFire, public firebaseservice: FirebaseService,
-    public appservice: AppService, public nav: NavController, public alertCtrl: AlertController) {
-    this.appservice.getUser().subscribe(user => {
-      this.currentUser = user;
-      let subscribe = this.firebaseservice.getUsersByUnitNumber(user.unitnumber).subscribe(users => {
-        users.forEach(usr => {
-          var userCouncilNames: string[] = [];
-          usr.councils.forEach(e => {
-            this.firebaseservice.getCouncilByKey(e).subscribe((councilObj) => {
-              councilObj.forEach(counObj => {
-                userCouncilNames.push(councilObj[0].council);
-                usr.councilnames = userCouncilNames.join(', ');
+  constructor(public af: AngularFire, public firebaseservice: FirebaseService, public nav: NavController, public alertCtrl: AlertController) {
+    // this.appservice.getUser().subscribe(user => {
+    this.userSubscription = this.af.auth.subscribe(auth => {
+      if (auth !== null) {
+        this.af.database.object('/users/' + auth.uid).subscribe(user => {
+          this.currentUser = user;
+          this.firebaseservice.getUsersByUnitNumber(user.unitnumber).subscribe(users => {
+            users.forEach(usr => {
+              var userCouncilNames: string[] = [];
+              usr.councils.forEach(e => {
+                this.firebaseservice.getCouncilByKey(e).subscribe((councilObj) => {
+                  councilObj.forEach(counObj => {
+                    userCouncilNames.push(councilObj[0].council);
+                    usr.councilnames = userCouncilNames.join(', ');
+                  });
+                });
               });
             });
+            this.users = users;
+            //subscribe.unsubscribe();
           });
         });
-        this.users = users;
-        subscribe.unsubscribe();
-      });
+      }
     });
   }
   cancel() {
