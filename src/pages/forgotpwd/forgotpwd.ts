ae0691d4906b5ced6844335286c067c1b72c1eed
tbharati <tbharati@prokarma.com>
2017-05-03 20:02:51 +0530

    code update

--- src/pages/forgotpwd/forgotpwd.ts
+++ src/pages/forgotpwd/forgotpwd.ts
@@ -17,41 +17,49 @@ import { EmailService } from '../../providers/emailservice';
 
 
 @Component({
-  selector: 'forgot-pwd',
-  templateUrl: 'forgotpwd.html',
-  providers: [EmailService]
+    selector: 'forgot-pwd',
+    templateUrl: 'forgotpwd.html',
+    providers: [EmailService]
 })
 
 
 
 
 export class ForgotPwd {
-    
-    email:any;
+
+    email: any;
     constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public alertCtrl: AlertController, public emailService: EmailService) { }
 
     sendEmail() {
 
-        if(this.email !== '') {
-             let loader = this.loadingCtrl.create({
-                spinner:'hide',
-            content: '<div class="circle-container"><div class="circleG_1"></div><div class="circleG_2"></div><div class="circleG_3"></div></div>',
+        if (this.email !== '') {
+            let loader = this.loadingCtrl.create({
+                spinner: 'hide',
+                content: '<div class="circle-container"><div class="circleG_1"></div><div class="circleG_2"></div><div class="circleG_3"></div></div>',
             });
 
             loader.present();
-            this.emailService.emailForgotPassword(this.email).subscribe(res => {
-                if (res.status === 200) {
-                    loader.dismiss();
-                    this.navCtrl.push(ForgotPwdSuccess);
-                }
-                else {
-                    loader.dismiss();
-                    alert("Mail sending Failed. Please check your email and try")
-                }
 
-            });
-            
-            
+
+            // Uncomment this code when email service works. 
+
+            // this.emailService.emailForgotPassword(this.email).subscribe(res => {
+            //     if (res.status === 200) {
+            //         loader.dismiss();
+            //         this.navCtrl.push(ForgotPwdSuccess);
+            //     }
+            //     else {
+            //         loader.dismiss();
+            //         alert("Mail sending Failed. Please check your email and try")
+            //     }
+            // });
+
+
+            //Comment this code when email service works.
+
+            loader.dismiss();
+            this.navCtrl.push(ForgotPwdSuccess);
+
         }
     }
 }
