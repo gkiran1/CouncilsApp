96d69481d77212a818a3f4b0b02830777c7454cf
vanjanishyam <vanjanisyam@prokarma.com>
2017-05-03 13:28:18 +0530

    committing code of changed menu screens.

--- src/pages/slide1/slide1.ts
+++ src/pages/slide1/slide1.ts
@@ -4,6 +4,9 @@ import { MenuPage } from '../menu/menu';
 import { AdminPage } from '../admin/admin.component';
 import { SettingsPage } from '../settings/settings';
 import { DonationsWelcomePage } from '../donations/donations-welcome/donations-welcome';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { Subscription } from "rxjs";
+import { FirebaseService } from '../../environments/firebase/firebase-service';
 
 @Component({
     templateUrl: 'slide1.html',
@@ -13,12 +16,27 @@ import { DonationsWelcomePage } from '../donations/donations-welcome/donations-w
 
 export class slide1Page {
     isAdmin: boolean = false;
+    userObj: FirebaseObjectObservable<any>;
+    userSubscription: Subscription;
     constructor(public nav: NavController,
+        private firebaseService: FirebaseService,
+        public af: AngularFire,
         public menuPage: MenuPage) {
         if (localStorage.getItem('isAdmin') === 'true') {
             this.isAdmin = true;
         }
-        console.log(this.isAdmin);
+        this.userObj = null;
+
+        this.userSubscription = this.af.auth.subscribe(auth => {
+            if (auth !== null) {
+                this.firebaseService.getUsersByKey(auth.uid).subscribe(usrs => {
+                    this.userObj = usrs[0];
+                    localStorage.setItem('unitType', usrs[0].unittype)
+                    localStorage.setItem('unitNumber', usrs[0].unitnumber.toString())
+                    localStorage.setItem('userCouncils', usrs[0].councils.toString())
+                });
+            };
+        });
     }
 
     setSelectedClass(button) {
