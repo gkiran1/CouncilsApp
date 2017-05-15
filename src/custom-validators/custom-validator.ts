aefb46a00939ca08d9882960586e1b65f6ece84d
tbharati <tbharati@prokarma.com>
2017-05-15 16:20:15 +0530

    email validation in donations page

--- src/custom-validators/custom-validator.ts
+++ src/custom-validators/custom-validator.ts
@@ -2,7 +2,7 @@ import { FormControl } from '@angular/forms';
 
 export function validateEmail(c: FormControl) {
 
-    var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    var EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (c.value != "" && (c.value.length <= 5 || !EMAIL_REGEXP.test(c.value))) {
         return { "incorrectMailFormat": true };
