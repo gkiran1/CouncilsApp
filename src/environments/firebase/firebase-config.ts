3c8b29702b632c0765266c467ffede6b11e50c22
tbharati <tbharati@prokarma.com>
2017-06-16 17:02:36 +0530

    config update

--- src/environments/firebase/firebase-config.ts
+++ src/environments/firebase/firebase-config.ts
@@ -11,10 +11,17 @@ export const FirebaseConfig = {
     // storageBucket: "lds-councils-8f56b.appspot.com",
     // messagingSenderId: "619253720821"
 
-    apiKey: "AIzaSyDXnywKsXMwYNmHtLjzQZxa52jhWrUAcD0",
-    authDomain: "councils-45092.firebaseapp.com",
-    databaseURL: "https://councils-45092.firebaseio.com",
-    projectId: "councils-45092",
-    storageBucket: "councils-45092.appspot.com",
-    messagingSenderId: "310008879834"
+    // apiKey: "AIzaSyDXnywKsXMwYNmHtLjzQZxa52jhWrUAcD0",
+    // authDomain: "councils-45092.firebaseapp.com",
+    // databaseURL: "https://councils-45092.firebaseio.com",
+    // projectId: "councils-45092",
+    // storageBucket: "councils-45092.appspot.com",
+    // messagingSenderId: "310008879834"
+
+    apiKey: "AIzaSyA7vboizvDrRORHvkT01Z8vKCOuH8In4UA",
+    authDomain: "councilsdb.firebaseapp.com",
+    databaseURL: "https://councilsdb.firebaseio.com",
+    projectId: "councilsdb",
+    storageBucket: "councilsdb.appspot.com",
+    messagingSenderId: "92555477583"
 }
