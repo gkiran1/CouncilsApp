a3621b22962fc46f862b9b7a362609cd6bb50ebc
PKRMSEZDC\kmanjunath <kmanjunath@prokarma.com>
2017-07-07 15:34:59 +0530

    Revert "firebaseconfig change"
    This reverts commit d180db412e9a5f7fb2953440eea7b9b1598f070f.

--- src/environments/firebase/firebase-config.ts
+++ src/environments/firebase/firebase-config.ts
@@ -18,17 +18,10 @@ export const FirebaseConfig = {
     // storageBucket: "councils-45092.appspot.com",
     // messagingSenderId: "310008879834"
 
-    // apiKey: "AIzaSyA7vboizvDrRORHvkT01Z8vKCOuH8In4UA",
-    // authDomain: "councilsdb.firebaseapp.com",
-    // databaseURL: "https://councilsdb.firebaseio.com",
-    // projectId: "councilsdb",
-    // storageBucket: "councilsdb.appspot.com",
-    // messagingSenderId: "92555477583"
-
-    apiKey: "AIzaSyBMZ0AOaVkjU41DLCGiY-loMzmAWQO24kI",
-    authDomain: "councils-donate-page.firebaseapp.com",
-    databaseURL: "https://councils-donate-page.firebaseio.com",
-    projectId: "councils-donate-page",
-    storageBucket: "councils-donate-page.appspot.com",
-    messagingSenderId: "259729083061" 
+    apiKey: "AIzaSyA7vboizvDrRORHvkT01Z8vKCOuH8In4UA",
+    authDomain: "councilsdb.firebaseapp.com",
+    databaseURL: "https://councilsdb.firebaseio.com",
+    projectId: "councilsdb",
+    storageBucket: "councilsdb.appspot.com",
+    messagingSenderId: "92555477583"
 }
