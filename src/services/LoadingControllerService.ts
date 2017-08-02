b97b5b4a7fc01e0a393da88b40dd306849e15f7d
tbharati <tbharati@prokarma.com>
2017-08-02 20:43:21 +0530

    Loading controller service update

--- src/services/LoadingControllerService.ts
+++ src/services/LoadingControllerService.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { LoadingController } from 'ionic-angular';
 
-
 @Injectable()
 export class LoadingControllerService {
-    loadingController: any;
 
-    constructor(private loadingCtrl: LoadingController) {
-        this.loadingController = this.loadingCtrl.create({           
-        });
+    constructor(private loadingCtrl: LoadingController) { }
+
+    getLoadingController() {
+        return this.loadingCtrl.create({});
     }
 }
